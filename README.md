8c997c9f86806814591b2911ac3484128a7fd778
Michelle Wang <michelle0306mm@gmail.com>
2022-10-21 15:06:22 -0700

    First version of AboutView

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AboutView
 
-A description of this package.
+Add an About page to your app.
+
+Simply add the AboutView() to where you want it to show up, set the AppName, AppVersion etc....
--- Sources/AboutView/AboutView.swift
+++ Sources/AboutView/AboutView.swift
@@ -1,6 +1,86 @@
-public struct AboutView {
-    public private(set) var text = "Hello, World!"
+//
+//  AboutView.swift
+//  about
+//
+//  Created by tim on 10/20/22.
+//
 
-    public init() {
+import SwiftUI
+import UIKit
+
+@available(iOS 14, *)
+struct AboutView: View {
+    @State var appIcon: UIImage = UIImage(systemName: "app")!
+    @State var appName: String  = "My App"
+    @State var appVersion: String = "v1.0 build 20"
+    @State var isDeveloperMode: Bool = false
+    @State var licenseText: String = ""
+    @State var privacyText: String = ""
+    @State var opensourceText: String = ""
+    @State var acknowledgementsText: String = ""
+    
+    @State var onDeveloperModeChanged: (Bool) -> Void = { isDeveloper in
+        print("default onDeveloperModeChanged called with \(isDeveloper)")
+    }
+    
+    public init(appName: String, appIcon: UIImage, appVersion: String , isDeveloperMode: Bool, licenseText: String, privacyText: String, opensourceText: String, acknowledgementsText:String) {
+        
+        _appName = State(initialValue: appName)
+        _appIcon = State(initialValue: appIcon)
+        _appVersion = State(initialValue: appVersion)
+        _isDeveloperMode = State(initialValue: isDeveloperMode)
+        _licenseText = State(initialValue: licenseText)
+        _privacyText = State(initialValue: privacyText)
+        _opensourceText = State(initialValue: opensourceText)
+        _acknowledgementsText = State(initialValue: acknowledgementsText)
+    }
+    
+    public var body: some View {
+        NavigationView {
+            VStack {
+            
+                // Product name & Icon
+                Image(uiImage: appIcon)
+                    .resizable()
+                    .frame(width:50,height:50)
+                
+                Text(appName).fontWeight(.black)
+                
+                // Version & Build number
+                Text(appVersion)
+                
+                List {
+                    // License agreement
+                    // Privacy agreement
+                    // Open source packages
+                    // 3rd Party Notices
+                    
+                    if licenseText != "" {
+                        NavigationLink(destination: ScrollView { Text(licenseText) }, label: { Text("License Agreement")} )
+                    }
+                    if privacyText != "" {
+                        NavigationLink(destination: ScrollView { Text(privacyText) }, label: { Text("Privacy Agreement")} )
+                    }
+                    if opensourceText != "" {
+                        NavigationLink(destination: ScrollView { Text(opensourceText) }, label: { Text("Open Source")} )
+                    }
+                    if acknowledgementsText != "" {
+                        NavigationLink(destination: ScrollView { Text(acknowledgementsText) }, label: { Text("Acknowledgements")} )
+                    }
+                    
+            
+                }
+                 
+                // Developer Mode
+                Toggle(isOn: $isDeveloperMode) {
+                    Text("Developer Mode")
+                }.padding()
+                    
+                Spacer()
+            }.navigationTitle("About")
+                .navigationBarTitleDisplayMode(.inline)
+        }.onChange(of: isDeveloperMode, perform: { newvalue in
+            onDeveloperModeChanged(newvalue)
+        })
     }
 }
